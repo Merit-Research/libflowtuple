ef17206da9d4c55961ca6b6cb05fe95d15f05903
Mark Weiman <mweiman@merit.edu>
2018-10-29 11:49:02 -0400

    flowtuple.h: add doxygen comments

--- lib/libflowtuple/flowtuple.h
+++ lib/libflowtuple/flowtuple.h
@@ -25,8 +25,36 @@
 extern "C" {
 #endif
 
-#include <inttypes.h>
+#include <inttypes.h>  /* uintX_t types */
 
+/*
+ * Flowtuple library
+ */
+
+/*
+ * Opaque structures
+ */
+
+/** Flowtuple handle object */
+typedef struct _flowtuple_handle_t flowtuple_handle_t;
+/** Flowtuple header object */
+typedef struct _flowtuple_header_t flowtuple_header_t;
+/** Flowtuple trailer/footer object */
+typedef struct _flowtuple_trailer_t flowtuple_trailer_t;
+/** Flowtuple interval start/end object */
+typedef struct _flowtuple_interval_t flowtuple_interval_t;
+/** Flowtuple class start/end object */
+typedef struct _flowtuple_class_t flowtuple_class_t;
+/** Flowtuple data object */
+typedef struct _flowtuple_data_t flowtuple_data_t;
+/** Flowtuple record object */
+typedef struct _flowtuple_record_t flowtuple_record_t;
+
+/*
+ * Enumerations
+ */
+
+/** Flowtuple record types. */
 typedef enum _flowtuple_record_type_t {
     FLOWTUPLE_RECORD_TYPE_NULL,
     FLOWTUPLE_RECORD_TYPE_HEADER,
@@ -36,75 +64,174 @@ typedef enum _flowtuple_record_type_t {
     FLOWTUPLE_RECORD_TYPE_FLOWTUPLE_DATA,
 } flowtuple_record_type_t;
 
-typedef struct _flowtuple_handle_t flowtuple_handle_t;
-typedef struct _flowtuple_header_t flowtuple_header_t;
-typedef struct _flowtuple_trailer_t flowtuple_trailer_t;
-typedef struct _flowtuple_interval_t flowtuple_interval_t;
-typedef struct _flowtuple_class_t flowtuple_class_t;
-typedef struct _flowtuple_data_t flowtuple_data_t;
-typedef struct _flowtuple_record_t flowtuple_record_t;
+/*
+ * Structures
+ */
 
+/** Slash eight structure. */
 typedef struct _flowtuple_slash_eight_t {
     uint8_t b;
     uint8_t c;
     uint8_t d;
 } flowtuple_slash_eight_t;
 
+
+/** Initialize flowtuple file for reading.
+ * @param filename Filename of input
+ * @return New flowtuple handle
+ */
 flowtuple_handle_t *flowtuple_initialize(const char *filename);
+
+/** Free a flowtuple handle structure.
+ * @param handle Handle to be freed
+ */
 void flowtuple_release(flowtuple_handle_t *handle);
 
 flowtuple_record_t *flowtuple_get_next(flowtuple_handle_t *handle);
 
+/** @addtogroup flowtuple_api_handle Options
+ * Libflowtuple handle getters
+ * @{
+ */
+
+/** Get file uri from handle object */
 const char *flowtuple_handle_get_uri(flowtuple_handle_t *handle);
+/** Get previous record retrieved (needs to be freed) */
 flowtuple_record_t *flowtuple_handle_get_last_record(flowtuple_handle_t *handle);
 
-flowtuple_record_type_t flowtuple_record_get_type(flowtuple_record_t *record);
+/** @} */
 
-flowtuple_header_t *flowtuple_record_get_header(flowtuple_record_t *record);
-flowtuple_trailer_t *flowtuple_record_get_trailer(flowtuple_record_t *record);
-flowtuple_interval_t *flowtuple_record_get_interval(flowtuple_record_t *record);
-flowtuple_class_t *flowtuple_record_get_class(flowtuple_record_t *record);
-flowtuple_data_t *flowtuple_record_get_data(flowtuple_record_t *record);
-
+/** Free a record object.
+ * @param record Record to be freed
+ */
 void flowtuple_record_free(flowtuple_record_t *record);
 
+/** @addtogroup flowtuple_api_record Record
+ * Libflowtuple record getters
+ * @{
+ */
+
+/** Get type of record */
+flowtuple_record_type_t flowtuple_record_get_type(flowtuple_record_t *record);
+
+/** Get header pointer from record */
+flowtuple_header_t *flowtuple_record_get_header(flowtuple_record_t *record);
+/** Get trailer/footer pointer from record */
+flowtuple_trailer_t *flowtuple_record_get_trailer(flowtuple_record_t *record);
+/** Get interval pointer from record */
+flowtuple_interval_t *flowtuple_record_get_interval(flowtuple_record_t *record);
+/** Get class pointer from record */
+flowtuple_class_t *flowtuple_record_get_class(flowtuple_record_t *record);
+/** Get data pointer from record */
+flowtuple_data_t *flowtuple_record_get_data(flowtuple_record_t *record);
+
+/** @} */
+
+/** @addtogroup flowtuple_api_header Header
+ * Libflowtuple header getters
+ * @{
+ */
+
+/** Get major version from header object */
 uint8_t flowtuple_header_get_version_major(flowtuple_header_t *header);
+/** Get minor version from header object */
 uint8_t flowtuple_header_get_version_minor(flowtuple_header_t *header);
+/** Get local initial time from header object */
 uint32_t flowtuple_header_get_local_init_time(flowtuple_header_t *header);
+/** Get length of interval from header object */
 uint16_t flowtuple_header_get_interval_length(flowtuple_header_t *header);
+/** Get trace uri string length from header object */
 uint16_t flowtuple_header_get_traceuri_len(flowtuple_header_t *header);
+/** Get trace uri from header object */
 const char *flowtuple_header_get_traceuri(flowtuple_header_t *header);
+/** Get number of plugins used from header object */
 uint16_t flowtuple_header_get_plugin_count(flowtuple_header_t *header);
+/** Get plugin magic numbers from header object */
 uint32_t *flowtuple_header_get_plugins(flowtuple_header_t *header);
 
+/** @} */
+
+/** @addtogroup flowtuple_api_trailer Trailer/Footer
+ * Libflowtuple trailer getters
+ * @{
+ */
+
+/** Get packet count from trailer object */
 uint64_t flowtuple_trailer_get_packet_count(flowtuple_trailer_t *trailer);
+/** Get accepted packet count from trailer object */
 uint64_t flowtuple_trailer_get_accepted_count(flowtuple_trailer_t *trailer);
+/** Get dropped packet count from trailer object */
 uint64_t flowtuple_trailer_get_dropped_count(flowtuple_trailer_t *trailer);
+/** Get time of first packet from trailer object */
 uint32_t flowtuple_trailer_get_first_packet_time(flowtuple_trailer_t *trailer);
+/** Get time of final packet from trailer object */
 uint32_t flowtuple_trailer_get_last_packet_time(flowtuple_trailer_t *trailer);
+/** Get final time of processing from trailer object */
 uint32_t flowtuple_trailer_get_local_final_time(flowtuple_trailer_t *trailer);
+/** Get runtime from trailer object */
 uint32_t flowtuple_trailer_get_runtime(flowtuple_trailer_t *trailer);
 
+/** @} */
+
+/** @addtogroup flowtuple_api_interval Interval
+ * Libflowtuple interval getters
+ * @{
+ */
+
+/** Get interval number from interval object */
 uint16_t flowtuple_interval_get_number(flowtuple_interval_t *interval);
+/** Get time from interval object */
 uint32_t flowtuple_interval_get_time(flowtuple_interval_t *interval);
 
+/** @} */
+
+/** @addtogroup flowtuple_api_class Class
+ * Libflowtuple class getters
+ * @{
+ */
+
+/** Get magic from class object */
 uint32_t flowtuple_class_get_magic(flowtuple_class_t *ftclass);
+/** Get class type from class object */
 uint16_t flowtuple_class_get_class_type(flowtuple_class_t *ftclass);
+/** Get key count from class object */
 uint32_t flowtuple_class_get_key_count(flowtuple_class_t *ftclass);
 
+/** @} */
+
+/** @addtogroup flowtuple_api_data Data
+ * Libflowtuple data getters
+ * @{
+ */
+
+/** Get class pointer from data object */
 flowtuple_class_t *flowtuple_data_get_class_start(flowtuple_data_t *data);
+/** Get number from data object */
 uint32_t flowtuple_data_get_number(flowtuple_data_t *data);
+/** Get source ip from data object */
 uint32_t flowtuple_data_get_src_ip(flowtuple_data_t *data);
+/** Get destination ip (integer) from data object */
 uint32_t flowtuple_data_get_dest_ip_int(flowtuple_data_t *data);
+/** Get destination ip (slash eight) from data object */
 flowtuple_slash_eight_t flowtuple_data_get_dest_ip_slash_eight(flowtuple_data_t *data);
+/** Get source port from data object */
 uint16_t flowtuple_data_get_src_port(flowtuple_data_t *data);
+/** Get destination port from data object */
 uint16_t flowtuple_data_get_dest_port(flowtuple_data_t *data);
+/** Get protocol from data object */
 uint8_t flowtuple_data_get_protocol(flowtuple_data_t *data);
+/** Get time to live from data object */
 uint8_t flowtuple_data_get_ttl(flowtuple_data_t *data);
+/** Get TCP flags from data object */
 uint8_t flowtuple_data_get_tcp_flags(flowtuple_data_t *data);
+/** Get IP length from data object */
 uint16_t flowtuple_data_get_ip_len(flowtuple_data_t *data);
+/** Get number of packets from data object */
 uint32_t flowtuple_data_get_packet_count(flowtuple_data_t *data);
+/** Is the destination IP in slash eight format? */
 int flowtuple_data_is_slash_eight(flowtuple_data_t *data);
+
+/** @} */
 
 #ifdef __cplusplus
 }
